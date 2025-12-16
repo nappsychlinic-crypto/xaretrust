90d602f44d11a5e5b320da77de6efbe06b5e352c
nappsychlinic-crypto <nap.psychlinic@gmail.com>
2025-12-16 07:33:59 +0530

    Vercel Fixes

--- src/app/api/contact/send-email/route.ts
+++ src/app/api/contact/send-email/route.ts
@@ -6,8 +6,8 @@ import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 const sesClient = new SESClient({
     region: process.env.AWS_REGION,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY || "",
-        secretAccessKey: process.env.AWS_SECRET_KEY || "",
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
     },
 });
 
